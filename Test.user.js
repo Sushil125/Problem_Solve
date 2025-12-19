cb1c21180a87f0a021e06c576e6948d95355ea8f
Sushil Amgain <50693709+Sushil125@users.noreply.github.com>
2025-12-19 16:38:02 +1100

    Bump version to 1.3 and update log message

--- Test.user.js
+++ Test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         My Auto-Updating Script
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  A script that automatically updates from GitHub
 // @author       Your Name
 // @match        https://example.com/*
@@ -14,5 +14,5 @@
     'use strict';
 
     // Your script code goes here
-    console.log("Hello! This is version 1.2 of My Auto-Updating Script. Its my second script and my third.");
+    console.log("Hello! This is version 1.3 of My Auto-Updating Script. Its my second script and my third.");
 })();
