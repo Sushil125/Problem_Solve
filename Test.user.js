3e2bfbba17517c145235fb6ffcbedee1b1c81f0e
Sushil Amgain <50693709+Sushil125@users.noreply.github.com>
2025-12-19 16:48:40 +1100

    Update Test.user.js

--- Test.user.js
+++ Test.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         My Auto-Updating Script
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  A script that automatically updates from GitHub
 // @author       Your Name
 // @match        https://example.com/*
@@ -11,7 +11,7 @@
 // ==/UserScript==
 
 (function() {
-    const msg = "✅ Script version 1.5 running!";
+    const msg = "✅ Script version 1.6 running!";
     console.log(msg);
     const banner = document.createElement('div');
     banner.textContent = msg;
